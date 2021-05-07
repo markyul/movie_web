c3cb04888f42f854cfcf1b96df08a58fc36f81ee
markyul <pig052656@naver.com>
2021-05-07 15:47:04 +0900

    Making the movie app - data fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -6,18 +8,35 @@ class App extends React.Component {
     movies: []
   }
 
+  getMovies = async() => {
+
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    console.log(movies);
+
+    this.setState({
+      movies,    // movies: movies와 같음
+      isLoading: false
+    });
+
+    // 이 방법은 나중에 별로일 듯
+    // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // console.log(movies.data.data.movies);
+  }
+
   // render 전에 동작
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 6000);
+  async componentDidMount() {
+    this.getMovies();
   }
 
   render() {
-    const {isLoading} = this.state;
+    const {isLoading, movies} = this.state;
     return (
       <div>
-        {isLoading ? "Loading" : "We are ready"}
+        {isLoading ? "Loading" : movies.map(movie => {
+        return (
+          <Movie key={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
+        );
+      })}
       </div>
     );
   }
